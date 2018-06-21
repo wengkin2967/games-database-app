931a8d56ad16621ba9485b005bc711e6749b77a2
Weng Kin <wengkin2967@gmail.com>
2018-06-22 01:28:42 +0800

    Added complete index page and started on new page

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var express = require("express"),
     methodOverride = require("method-override"),
     expressSanitizer = require("express-sanitizer");
 
+// jQuery Set up
+var jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+var $ = require("jquery")(window);
+
 // App Config
 mongoose.connect("mongodb://localhost/game-database-app");
 app.set("view engine","ejs");
@@ -13,17 +21,18 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(expressSanitizer());
 
+
 // Mongoose/Model Config
 var gameSchema = new mongoose.Schema({
   title: String,
   image: String,
   publisher: String,
-  released: {type: Date, default: Date.now},
+  released: Date,
   body: String,
   created: {type: Date, default: Date.now}
 });
 
-var Blog = mongoose.model("Game", gameSchema);
+var Game = mongoose.model("Game", gameSchema);
 
 app.get("/", function(req,res){
     res.redirect("/games");
@@ -31,8 +40,20 @@ app.get("/", function(req,res){
 
 // Index Route
 app.get("/games", function(req,res){
-    res.render("index");
+    Game.find({}, function(err,games){
+        if(err){
+            alert("Cannot find games");
+        }
+        else{
+            res.render("index", {games:games});
+        }
+    })
 })
+
+// New Route
+app.get("/games/new", function(req,res){
+    res.render("new");
+});
 
 app.listen(3000, function(){
     console.log("Games Database has started");
